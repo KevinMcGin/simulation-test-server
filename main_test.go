981e9ef9ad127897ed8fc56d9a048c19b717a703
Kevin McGinley <kevinmcginley@cityswift.com>
2024-08-29 17:19:43 +0100

    Add test

---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "testing"
+    "regexp"
+)
+
+// TestHelloName calls greetings.Hello with a name, checking
+// for a valid return value.
+func TestGetFolderName(t *testing.T) {
+    folderName := getFolderName()
+	match, _ := regexp.MatchString("^[0-9a-f]+$", folderName)
+	if !match {
+		t.Fatalf(`getFolderName() = %v; want a string of 0-9a-f`, folderName)
+	}
+}
